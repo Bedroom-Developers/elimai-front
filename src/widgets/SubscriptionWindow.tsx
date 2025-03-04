f4f9b3f7ccddd57affa927433ccc7fcb9a80905f
iufb <bashirov3ld2@gmail.com>
2025-03-04 15:34:49 +0500

    disable buy sub

--- src/widgets/SubscriptionWindow.tsx
+++ src/widgets/SubscriptionWindow.tsx
@@ -4,7 +4,6 @@ import { rBuyTicket, rGetSubscriptionCount } from "@/shared/api/games"
 import { useAuth } from "@/shared/context"
 import { showErrorNotification } from "@/shared/notifications"
 import { Alert, Box, Button, Group, Input, Skeleton, Stack, Text } from "@mantine/core"
-import { getCookie } from "cookies-next"
 import { AlertTriangle } from "lucide-react"
 import { useTranslations } from "next-intl"
 import { useParams } from "next/navigation"
@@ -91,7 +90,7 @@ const Form = () => {
         <Text c="slate.6">{t('buy.subCount', { count })} </Text>
 
     const onSubmit: SubmitHandler<{ tel: string }> = (data) => {
-        mutate({ data: { TELEPHONE: data.tel.replace(/[()\s-]/g, ""), EMAIL: getCookie('email'), COUNT: 1, EVENT_ID: 1, LOCALE: locale as string, TYPE: 'Aboniment' } })
+        // mutate({ data: { TELEPHONE: data.tel.replace(/[()\s-]/g, ""), EMAIL: getCookie('email'), COUNT: 1, EVENT_ID: 1, LOCALE: locale as string, TYPE: 'Aboniment' } })
 
     };
     return <form onSubmit={handleSubmit(onSubmit)}>
