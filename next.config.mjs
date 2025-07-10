bcc290b5cd5bc3ecec692d97c5a2e59eb5c26add
yerkow <yekinsgn@proton.me>
2025-07-10 14:47:08 +0500

    upd: sentry enabled

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,20 @@
-import createNextIntlPlugin from 'next-intl/plugin';
+import { withSentryConfig } from "@sentry/nextjs";
+import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin()
+const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    output: 'standalone'
-}
-export default withNextIntl(nextConfig)
-
+  reactStrictMode: false,
+  output: "standalone",
+};
+export default withSentryConfig(withNextIntl(nextConfig), {
+  org: "sentry",
+  project: "fc-elimai-ui",
+  sentryUrl: "https://sentry.ispark.kz/",
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  tunnelRoute: "/monitoring",
+  disableLogger: true,
+  automaticVercelMonitors: true,
+});
