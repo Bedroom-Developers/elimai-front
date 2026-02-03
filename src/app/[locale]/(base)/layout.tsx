021358c1da6ae75bef582f410add074bdd4b6615
iufb <bashirov3ld2@gmail.com>
2026-02-03 16:31:40 +0500

    ELM-9 feat: add footer with policy link and copyright information to layout for improved user accessibility

--- src/app/[locale]/(base)/layout.tsx
+++ src/app/[locale]/(base)/layout.tsx
@@ -1,4 +1,5 @@
 import { getTranslations } from "next-intl/server"
+import { Link } from "@/i18n/routing"
 import { Header } from "../_components/Header"
 import { PartnersSection } from "../_components/PartnersSection"
 
@@ -10,6 +11,20 @@ export default async function Layout({ params, children }: { params: { locale: s
             {children}
         </main>
         <PartnersSection />
+          <footer>
+            <div className="mx-auto max-w-[1200px] p-2.5 sm:p-2.5 xl:p-0">
+                <Link
+                    href="/policy"
+                    className="block text-center text-gray-500 lg:text-start"
+                >
+                    {t("policy.title")}
+                </Link>
+                <p className="py-2.5 text-center text-sm text-gray-500">
+                    © {new Date().getFullYear()} {t("footer")}
+                </p>
+            </div>
+        </footer>
+
     </section>
 }
 
