c51ae14876021263508fb1aa953424fc7cb1b248
iufb <bashirov3ld2@gmail.com>
2026-02-20 12:45:29 +0500

    chore: remove outdated image templates and replace background image with new webp format

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import LocaleSwitcher from "../_components/LocaleSwitcher";
 export default async function RootLayout({ children }: { children: any }) {
     return <>
         <LocaleSwitcher className="absolute top-4 right-4 bg-white" />
-        <main className="bg-[url('/bg-item.png')]">
+        <main className="bg-[url('/bg.webp')]">
             {children}
         </main>
     </>
